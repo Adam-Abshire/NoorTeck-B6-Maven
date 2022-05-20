73a06ba3b7faffcefdfcf8342591aca85d1c16cd
Adam-Abshire <71105578+Adam-Abshire@users.noreply.github.com>
2022-05-19 20:38:09 -0400

    Initial Commit

---
+++ src/test/java/steps/XYZSmokeStep.java
@@ -0,0 +1,22 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+
+public class XYZSmokeStep {
+	
+	@Given("User navigates to login page")
+	public void natLogin() {
+		System.out.println("User able to login...");
+		System.out.println("User able to login...");
+		System.out.println("User able to login...");
+		System.out.println("User able to login...");
+		System.out.println("User able to login...");
+		System.out.println("User able to login...");
+		System.out.println("User able to login...");
+		System.out.println("User able to login...");
+		System.out.println("User able to login...");
+		
+		
+	}
+
+}
---
+++ src/test/resources/orangehrm/SmokeTest.feature
@@ -0,0 +1,4 @@
+Feature: This is the feature
+
+Scenario: Verify user able to login successfully
+	Given: User navigates to login page
